da41203864c6616fe349d8fc75396f2c525f1df3
Yifan Wu <tyivanwu@gmail.com>
2017-05-17 14:56:35 -0400

    more formal jQuery

--- js/subjpop.js
+++ js/subjpop.js
@@ -1,3 +1,5 @@
 // open external link in a new tab
-$('a[href^="http://"]').attr('target', '_blank')
-$('a[href^="https://"]').attr('target', '_blank')
+$(document).ready(function () {
+    $('a[href^="http://"]').attr('target', '_blank')
+    $('a[href^="https://"]').attr('target', '_blank')
+});
