932e6e97de88bb6407dde85d0c5d083c3a917b8e
Yifan Wu <tyivanwu@gmail.com>
2017-05-17 21:52:27 -0400

    rename js function

--- js/subjpop.js
+++ js/subjpop.js
@@ -1,5 +1,9 @@
-// open external link in a new tab
 $(document).ready(function () {
+    openInNewTab();
+});
+
+// open external link in a new tab
+function openInNewTab() {
     $('a[href^="http://"]').attr('target', '_blank')
     $('a[href^="https://"]').attr('target', '_blank')
-});
+}
