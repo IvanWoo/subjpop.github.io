96cb54adb1bc541fdc16779e1a88405081b83542
Yifan Wu <tyivanwu@gmail.com>
2018-10-28 23:02:41 -0400

    mark the latest episodes

--- js/subjpop.js
+++ js/subjpop.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     openInNewTab();
+    markLatest();
 });
 
 // open external link in a new tab
@@ -13,4 +14,15 @@ const openInNewTab = () => {
     // $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
     // $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
 }
-}
+
+const markLatest = () => {
+    const allDates = document.querySelectorAll("time");
+    const days = 30;
+    // days => ms
+    const criteria = days * 24 * 60 * 60 * 1000;
+    allDates.forEach(function(element) {
+        if ((Date.now() - Date.parse(element.dateTime)) < criteria) {
+            element.parentElement.parentElement.classList.add("latest");
+        }
+    });
+};
