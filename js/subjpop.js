50c406dbf2028e8d7da704f2d5b0c2d8edeacf35
Yifan Wu <tyivanwu@gmail.com>
2018-10-28 23:01:11 -0400

    remove jQuery dependency for most pages

--- js/subjpop.js
+++ js/subjpop.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     openInNewTab();
 });
 
 // open external link in a new tab
-function openInNewTab() {
-    $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
-    $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
+const openInNewTab = () => {
+    document.querySelectorAll('a').forEach(function (a) {
+        if (! a.href.startsWith(document.location.origin)) {
+            a.setAttribute('target', '_blank');
+            a.setAttribute("rel", "noopener");
+        }
+    })
+    // $('a[href^="http://"]').attr('target', '_blank').attr('rel', 'noopener')
+    // $('a[href^="https://"]').attr('target', '_blank').attr('rel', 'noopener')
+}
 }
