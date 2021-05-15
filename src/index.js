d7128b2a339c4502080d7576a34abed9f0bb5d9a
Yifan Wu <tyivanwu@gmail.com>
2021-05-14 21:36:09 -0400

    chore: clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Chaffle from "chaffle";
 import lazySizes from "lazysizes";
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", () => {
   openInNewTab();
   markLatest();
   scrambleAuthor();
@@ -9,35 +9,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // open external link in a new tab
 const openInNewTab = () => {
-  document.querySelectorAll('a').forEach(function (a) {
+  document.querySelectorAll("a").forEach((a) => {
     if (!a.href.startsWith(document.location.origin)) {
-      a.setAttribute('target', '_blank');
+      a.setAttribute("target", "_blank");
       a.setAttribute("rel", "noopener");
     }
-  })
-}
+  });
+};
 
 const markLatest = () => {
   const allDates = document.querySelectorAll("time");
   const days = 30;
   // days => ms
   const criteria = days * 24 * 60 * 60 * 1000;
-  allDates.forEach(function (element) {
-    if ((Date.now() - Date.parse(element.dateTime)) < criteria) {
+  allDates.forEach((element) => {
+    if (Date.now() - Date.parse(element.dateTime) < criteria) {
       element.parentElement.parentElement.classList.add("latest");
     }
   });
 };
 
 const scrambleAuthor = () => {
-  const elements = document.querySelectorAll('[data-chaffle]');
-  Array.prototype.forEach.call(elements, function (el) {
+  const elements = document.querySelectorAll("[data-chaffle]");
+  Array.prototype.forEach.call(elements, (el) => {
     const chaffle = new Chaffle(el, {
       speed: 10,
       delay: 20,
     });
-    el.addEventListener('mouseover', function () {
+    el.addEventListener("mouseover", () => {
       chaffle.init();
     });
   });
-}
+};
