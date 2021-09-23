719d15e9bfa70b0200192201f4d400c30d22b348
Yifan Wu <tyivanwu@gmail.com>
2021-09-23 18:02:33 -0400

    chore: better tests

--- spec/lib/podcast_hosts_spec.rb
+++ spec/lib/podcast_hosts_spec.rb
@@ -3,17 +3,19 @@
 require_relative '../../lib/podcast_hosts'
 
 RSpec.describe Lizhi do
-  it 'should have stream_table' do
+  it 'has stream_table' do
     lizhi = described_class.new(LIZHI_RADIO_ID)
     stream_table = lizhi.stream_table
+    expect(stream_table.keys).to all(match('www.lizhi.fm'))
     expect(stream_table.length).to satisfy('be positive') { |v| v.positive? }
   end
 end
 
 RSpec.describe Ximalaya do
-  it 'should have stream_table' do
+  it 'has stream_table' do
     ximalaya = described_class.new(XIMALAYA_ALBUM_ID)
     stream_table = ximalaya.stream_table
+    expect(stream_table.keys).to all(match('www.ximalaya.com/yinyue'))
     expect(stream_table.length).to satisfy('be positive') { |v| v.positive? }
   end
 end
