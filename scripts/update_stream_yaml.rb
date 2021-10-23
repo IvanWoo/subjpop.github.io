fba9b6d2595395331b38de4338710fce538a57cf
Yifan Wu <tyivanwu@gmail.com>
2021-10-23 11:49:17 -0400

    perf: update stream concurrently

--- scripts/update_stream_yaml.rb
+++ scripts/update_stream_yaml.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'concurrent'
+
 require_relative '../lib/podcast_hosts'
 
 ROOT_DIR = File.expand_path('..', __dir__)
@@ -7,6 +9,7 @@ STREAM_YAML = "#{ROOT_DIR}/_data/stream_yaml.yml"
 
 def output(stream_table)
   old_stream_table = YAML.safe_load(File.read(STREAM_YAML))
+  old_stream_table = {} if old_stream_table.nil?
   stream_table = old_stream_table.merge(stream_table)
   File.open(STREAM_YAML, 'w') do |file|
     file.write stream_table.to_yaml
@@ -15,11 +18,36 @@ def output(stream_table)
   puts '💎 rss feed generated successfully'
 end
 
-def main
+def merge_stream_tables(stream_tables)
+  stream_table = {}
+  stream_tables.each do |stream_table_item|
+    stream_table.merge(stream_table_item)
+  end
+  stream_table
+end
+
+def main_fast
+  pool = Concurrent::ThreadPoolExecutor.new(max_threads: 4)
+  podcast_hosts = [Lizhi.new, Ximalaya.new]
+  stream_tables = podcast_hosts.map do |podcast_host|
+    Concurrent::Future.execute({ executor: pool }) do
+      podcast_host.stream_table
+    end
+  end.map(&:value)
+
+  stream_table = merge_stream_tables(stream_tables)
+  output(stream_table)
+end
+
+def main_slow
   lizhi = Lizhi.new
   ximalaya = Ximalaya.new
   stream_table = ximalaya.stream_table.merge(lizhi.stream_table)
   output(stream_table)
+end
+
+def main
+  main_fast
 end
 
 main
