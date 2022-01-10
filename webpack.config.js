ccfa0de1d3a3f4397f767c40d2fe74259f0eda59
Yifan Wu <tyivanwu@gmail.com>
2022-01-09 23:42:39 -0500

    build: integrate typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "./js"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+    entry: {
+        main: path.resolve(__dirname, "./src/index.js"),
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "./js"),
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            { test: /\.tsx?$/, loader: "ts-loader" },
+        ],
+    },
 };
