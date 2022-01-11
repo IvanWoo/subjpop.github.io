f5ddd4653916fc365740e5b83ad448a87d1e069c
Yifan Wu <tyivanwu@gmail.com>
2022-01-10 22:48:57 -0500

    feat: add the search app

---
+++ src/search/query.ts
@@ -0,0 +1,22 @@
+import { pickRandom } from "@thi.ng/random";
+
+const QUERIES = [
+    "mimida",
+    "stwind",
+    "dvc",
+    "大胃麒麟",
+    "赵老师",
+    "南瓜",
+    "kitashin",
+    "您好",
+    "小水",
+    "kohh",
+    "zombie",
+    "ayano",
+    "tofu",
+    "rap",
+    "乐曲",
+    "偶像",
+];
+
+export const INITIAL_QUERY = pickRandom(QUERIES);
